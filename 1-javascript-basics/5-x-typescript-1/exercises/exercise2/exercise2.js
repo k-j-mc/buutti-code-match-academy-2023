baae1c6d4f9d2ec21362d50e3a3d967ef057a489
k-j-mc <kristofermcc@gmail.com>
2023-09-21 09:33:11 +0300

    5-typescript basics exercises complete

---
+++ 1-javascript-basics/5-x-typescript-1/exercises/exercise2/exercise2.js
@@ -0,0 +1,11 @@
+var a = 500;
+var b = 50;
+if (a > b) {
+	console.log("A is greater");
+} else if (a < b) {
+	console.log("B is greater");
+} else if (a === b) {
+	console.log("A and B are equal");
+} else {
+	console.log("error");
+}
---
+++ 1-javascript-basics/5-x-typescript-1/exercises/exercise2/exercise2.ts
@@ -0,0 +1,12 @@
+const a: number = 500;
+const b: number = 50;
+
+if (a > b) {
+	console.log("A is greater");
+} else if (a < b) {
+	console.log("B is greater");
+} else if (a === b) {
+	console.log("A and B are equal");
+} else {
+	console.log("error");
+}
